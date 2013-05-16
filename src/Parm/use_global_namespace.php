5153ba5b1d6988bb1b1cf8e3456ca2ec7dd4e9a5
Andrew Cassell <alc277@gmail.com>
2013-05-16 00:02:37 -0400

    use global namespace if desired

--- src/Parm/use_global_namespace.php
+++ src/Parm/use_global_namespace.php
@@ -1,5 +1,17 @@
 <?php
 
+/*
+ * This file is part of Parm. It allows you to more easily use Parm in the global namespace.
+ * Just include this file to class alias the most frequently used classes.
+ * This does have the potential to case a conflict with another library.
+ *
+ * (c) Andrew Cassell <me@andrewcassell.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ * 
+ */
+
 class_alias('Parm\DatabaseProcessor', 'DatabaseProcessor');
 class_alias('Parm\DatabaseNode', 'DatabaseNode');
 
