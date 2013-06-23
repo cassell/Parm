799e02e05b3a875622514c1954a081ebeade16d6
Andrew Cassell <alc277@gmail.com>
2013-06-23 18:56:34 -0400

    migrating more of the generator

--- src/Parm/Generator/DatabaseGenerator.php
+++ src/Parm/Generator/DatabaseGenerator.php
@@ -4,13 +4,20 @@ namespace Parm\Generator;
 
 class DatabaseGenerator
 {
+	const DestinationDirectoryFolderPermissions = 0777;
+	const GenereatedCodeFilePermissions = 0777;
+	
+	
 	var $databaseNode;
 	var $destinationDirectory;
-	var $generatedNamespace;
+	
+	var $generatedNamespace = "\\Parm\\Dao\\";
+
 	
 	function __construct($databaseNode)
 	{
 		$this->setDatabaseNode($databaseNode);
+		$this->setGeneratedNamespace("\\Parm\\Dao\\");
 	}
 	
 	function setDatabaseNode($databaseNode)
@@ -52,13 +59,148 @@ class DatabaseGenerator
 		return $tableNames;
 	}
 	
-	function generate()
+	private function getColumns($tableName)
 	{
-		$tableNames = $this->getTableNames();
+		$dp = new \Parm\DatabaseProcessor($this->databaseNode);
+		$dp->setSQL("SHOW COLUMNS FROM " . $dp->escapeString($this->databaseNode->serverDatabaseName) . "." . $dp->escapeString($tableName));
+		return $dp->getArray();
+	}
+	
+	private function getTemplatingDataFromTableName($tableName)
+	{
 		
-		print_r($tableNames);
+		$idFieldName = '';
+		$defaultValuePack = array();
+		$fieldsPack = array();
+		$bindingsPack = array();
+		
+		$className = ucfirst(\Parm\DataAccessArray::columnToCamelCase($tableName));
+		
+		// get columns
+		$columns = $this->getColumns($tableName);
+		
+		// id field
+		if($columns != null && count($columns) > 0)
+		{
+			foreach($columns as $key => $column)
+			{
+				if($column['Key'] == "PRI")
+				{
+					$columns[$key]['primaryKey'] = 1;
+					$columns[$key]['notPrimaryKey'] = 0;
+					$idFieldName = $column['Field'];
+				}
+				else
+				{
+					$columns[$key]['primaryKey'] = 0;
+					$columns[$key]['notPrimaryKey'] = 1;
+				}
+				
+				$fieldsPack[] = "'" . $column['Field'] . "'";
+				
+				$columns[$key]['FieldCase'] = ucfirst(\Parm\DataAccessArray::columnToCamelCase($column['Field']));
+				
+				if($column['Type'] == "datetime" || $column['Type'] == "date")
+				{
+					$columns[$key]['dateTimeField'] = 1;
+				}
+				else
+				{
+					$columns[$key]['dateTimeField'] = 0;
+				}
+				
+				if($column['Default'] == null)
+				{
+					$defaultValuePack[] = "'" . $column['Field'] . "' => null";
+				}
+				else
+				{
+					$defaultValuePack[] = "'" . $column['Field'] . "' => '" . str_replace("'","\'",$column['Default']) . "'";
+				}
+				
+				if($column['Type'] == "tinyint(1)" || $column['Type'] == "int(1)")
+				{
+					$bindingsPack[] = "\tfinal function add" . ucfirst(\Parm\DataAccessArray::columnToCamelCase($column['Field'])) . "TrueBinding(){ \$this->addBinding(new TrueBooleanBinding('" . $tableName . "." . $column['Field'] . "')); }";
+					$bindingsPack[] = "\tfinal function add" . ucfirst(\Parm\DataAccessArray::columnToCamelCase($column['Field'])) . "FalseBinding(){ \$this->addBinding(new FalseBooleanBinding('" . $tableName . "." . $column['Field'] . "')); }";
+					$bindingsPack[] = "\tfinal function add" . ucfirst(\Parm\DataAccessArray::columnToCamelCase($column['Field'])) . "NotTrueBinding(){ \$this->addBinding(new NotEqualsBinding('" . $tableName . "." . $column['Field'] . "',1)); }";
+					$bindingsPack[] = "\tfinal function add" . ucfirst(\Parm\DataAccessArray::columnToCamelCase($column['Field'])) . "NotFalseBinding(){ \$this->addBinding(new NotEqualsBinding('" . $tableName . "." . $column['Field'] . "',0));  }";
+					$bindingsPack[] = "\n";
+				}
+			}
+		}
+		
+		return array( 'tableName' => $tableName,
+					   'variableName' => \Parm\DataAccessArray::columnToCamelCase($tableName),
+					   'className' => $className,
+					   'databaseName' => $this->databaseNode->serverDatabaseName,
+					   'idFieldName' => $idFieldName,
+					   'columns' => $columns,
+					   'defaultValuePack' => implode(", ",$defaultValuePack),
+					   'fieldList' => implode(", ", $fieldsPack),
+					   'bindingsPack' => implode("\n", $bindingsPack)
+				    );
 		
 	}
+	
+	
+	function generate()
+	{
+		if($this->destinationDirectory == null)
+		{
+			throw new Exception('Destination directory required');
+		}
+		
+		if(!file_exists($this->destinationDirectory))
+		{
+			if(!@mkdir($this->destinationDirectory))
+			{
+				throw new Exception('Unable to create database destination directory "' . htmlentities($this->destinationDirectory) . '".');
+			}
+			try
+			{
+				chmod($this->destinationDirectory,self::DestinationDirectoryFolderPermissions);
+			}
+			catch(Exception $e)
+			{
+				throw new Exception('Unable to make database destination directory "' . htmlentities($this->destinationDirectory) . '" writeable.');
+			}
+		}
+		
+		$tableNames = $this->getTableNames();
+		
+		foreach($tableNames as $tableName)
+		{
+			print_r($this->getTemplatingDataFromTableName($tableName));
+		}
+		
+//		print_r($tableNames);
+		
+	}
+	
+	private function writeContentsToFile($fileName,$contents)
+	{
+		if(file_exists($fileName) && !is_writable($fileName))
+		{
+			throw new Exception('File is unwritable: ' . $fileName);
+		}
+		else if(@file_put_contents($fileName,$contents) !== FALSE)
+		{
+			try
+			{
+				@chmod($fileName,self::GenereatedCodeFilePermissions);
+			}
+			catch(Exception $e)
+			{
+				throw new Exception('Unable to make file "' . htmlentities($fileName) . '" read/write by all.');
+			}
+			return true;
+		}
+		else
+		{
+			throw new Exception('Unable to write file: ' . htmlentities($fileName));
+		}
+	}
+	
 	
 	
 	
