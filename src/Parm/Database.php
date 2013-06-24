7c6a3bee0b23008b12ee3da4147af5a35d886f08
Andrew Cassell <alc277@gmail.com>
2013-06-24 16:56:09 -0400

    Exception bugfix

--- src/Parm/Database.php
+++ src/Parm/Database.php
@@ -26,7 +26,7 @@ class Database
 		}
 		else
 		{
-			throw new Exception('Master DatabaseNode must be a Parm\DatabaseNode');
+			throw new \Exception('Master DatabaseNode must be a Parm\DatabaseNode');
 		}
 	}
 	
