235d76e108d51f2ecbf6e291caf0856ba69c4e08
Andrew Cassell <alc277@gmail.com>
2013-06-26 01:01:34 -0400

    PHPunit

---
+++ tests/DatabaseConnectionTest.php
@@ -0,0 +1,16 @@
+<?php
+
+require '../vendor/autoload.php';
+
+class DatabaseConnectionTest extends PHPUnit_Framework_TestCase
+{
+	public function testConnection()
+	{
+		$this->assertEquals(5, 5);
+	}
+	
+	
+}
+
+
+?>
