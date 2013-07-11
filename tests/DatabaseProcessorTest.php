dfcdec1521515ce181bc9288a662768c2aecb57e
Andrew Cassell <alc277@gmail.com>
2013-07-11 12:15:26 -0400

    test unbuffered process, and outputJSONString

--- tests/DatabaseProcessorTest.php
+++ tests/DatabaseProcessorTest.php
@@ -118,42 +118,19 @@ class DatabaseProcessorTest extends PHPUnit_Framework_TestCase
 		$this->assertEquals("18503",$dp->getFirstField("zipcode"));
 	}
 	
-	/*
-	
-	// using unbuffered mysql queries
-	function unbufferedProcess()
+	public function testOutputJSONString()
 	{
-		$dp = new DatabaseProcessor('sqlicious_test');
-		$dp->setSQL("select * from zipcodes");
-		
-		$sumZipcodes = 0;
-		
-		$dp->unbufferedProcess(function($obj) use (&$sumZipcodes)
-		{
-			$sumZipcodes += $obj['zipcode'];
-		});
-				
-		\Enhance\Assert::areIdentical($sumZipcodes, 1596092501);	
-	}
-	
-	function outputJSONString()
-	{
-		$dp = new DatabaseProcessor('sqlicious_test');
-		$dp->setSQL("select * from zipcodes where city = 'Herndon' order by state asc limit 3");
+		$dp = new Parm\DatabaseProcessor('parm_tests');
+		$dp->setSQL("SELECT * FROM `zipcodes` WHERE `city` LIKE '%Freedom%';");
 		
 		ob_start();
 		
 		$dp->outputJSONString();
 				
 		$json = ob_get_clean();
-				
-		\Enhance\Assert::areIdentical('[{"zipcodeId":"10405","zipcode":"67739","state":"KS","longitude":"-100.786870000000","latitude":"39.893743000000","city":"Herndon","stateName":"Kansas"},{"zipcodeId":"11113","zipcode":"42236","state":"KY","longitude":"-87.599350000000","latitude":"36.705024000000","city":"Herndon","stateName":"Kentucky"},{"zipcodeId":"25190","zipcode":"17830","state":"PA","longitude":"-76.805130000000","latitude":"40.690647000000","city":"Herndon","stateName":"Pennsylvania"}]', $json);
+		
+		$this->assertEquals('[{"zipcodeId":"446","zipcode":"16637","state":"PA","longitude":"-78.433010000000","latitude":"40.340680000000","city":"East Freedom","stateName":"Pennsylvania"},{"zipcodeId":"567","zipcode":"15042","state":"PA","longitude":"-80.232080000000","latitude":"40.682566000000","city":"Freedom","stateName":"Pennsylvania"},{"zipcodeId":"1099","zipcode":"17349","state":"PA","longitude":"-76.681120000000","latitude":"39.753369000000","city":"New Freedom","stateName":"Pennsylvania"}',$json);
 	}
-
-
-
-	*/
-	
 	
 }
 
