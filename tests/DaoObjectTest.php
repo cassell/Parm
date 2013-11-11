b7870c1f373d97582131721c333b27e48378917d
Andrew Cassell <alc277@gmail.com>
2013-11-11 17:38:21 -0500

    modified test case

--- tests/DaoObjectTest.php
+++ tests/DaoObjectTest.php
@@ -105,32 +105,6 @@ class DaoObjectTest extends PHPUnit_Framework_TestCase
 	function testInsertNewObject()
 	{
 		$new = new ParmTests\Dao\PeopleDaoObject();
-		$new->setFirstName("Núñez"); // spanish
-		$new->setLastName("κόσμε"); // greek
-		$new->setCreateDate(time());
-		$new->setCreateDatetime(time());
-		$new->setZipcodeId(1529);
-		$new->setArchived(0);
-		$new->save();
-		
-		$test = ParmTests\Dao\PeopleDaoObject::findId($new->getId());
-		
-		if($test != null)
-		{
-			$this->assertEquals("Núñez", $test->getFirstName());
-			$this->assertEquals("κόσμε", $test->getLastName());
-		}
-		else
-		{
-			$this->fail();
-		}
-		
-	}
-	/*
-	
-	function testUTF8Saving()
-	{
-		$new = new ParmTests\Dao\PeopleDaoObject();
 		$new->setFirstName("Coach");
 		$new->setLastName("Parmo");
 		$new->setCreateDate(time());
@@ -152,6 +126,31 @@ class DaoObjectTest extends PHPUnit_Framework_TestCase
 		
 	}
 	
+	function testUTF8Saving()
+	{
+		$new = new ParmTests\Dao\PeopleDaoObject();
+		$new->setFirstName("Núñez"); // spanish
+		$new->setLastName("κόσμε"); // greek
+		$new->setCreateDate(time());
+		$new->setCreateDatetime(time());
+		$new->setZipcodeId(1529);
+		$new->setArchived(0);
+		$new->save();
+		
+		$test = ParmTests\Dao\PeopleDaoObject::findId($new->getId());
+		
+		if($test != null)
+		{
+			$this->assertEquals("Núñez", $test->getFirstName());
+			$this->assertEquals("κόσμε", $test->getLastName());
+		}
+		else
+		{
+			$this->fail();
+		}
+		
+	}
+	
 	function testJSONNewObject()
 	{
 		$new = new ParmTests\Dao\PeopleDaoObject();
@@ -160,7 +159,7 @@ class DaoObjectTest extends PHPUnit_Framework_TestCase
 		$new->setZipcodeId(555);
 		$new->setArchived(0);
 		
-		$this->assertEquals('a:9:{s:8:"peopleId";N;s:9:"firstName";s:5:"James";s:8:"lastName";s:8:"Buchanan";s:9:"zipcodeId";i:555;s:8:"archived";i:0;s:12:"testDataBlob";N;s:10:"createDate";N;s:14:"createDatetime";N;s:2:"id";N;}', serialize($new->toJSON()));
+		$this->assertEquals('a:10:{s:8:"peopleId";N;s:9:"firstName";s:5:"James";s:8:"lastName";s:8:"Buchanan";s:9:"zipcodeId";i:555;s:8:"archived";N;s:12:"testDataBlob";N;s:10:"createDate";N;s:14:"createDatetime";N;s:15:"createTimestamp";N;s:2:"id";N;}', serialize($new->toJSON()));
 		
 	}
 	
@@ -176,7 +175,6 @@ class DaoObjectTest extends PHPUnit_Framework_TestCase
 		$this->assertEquals('{"zipcodeId":"555","zipcode":"17224","state":"PA","longitude":"-77.906230000000","latitude":"39.957564000000","archived":"0","city":"Fort Loudon","stateName":"Pennsylvania","id":"555"}',$buchananBirthplace->toJSONString());
 	}
 
-	*/
 	
 	
 }
