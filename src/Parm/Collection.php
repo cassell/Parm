1f3ac721b295679fc32306c5eb2e7b0675385766
Andrew Cassell <alc277@gmail.com>
2014-04-18 10:54:35 -0400

    docs

--- src/Parm/Collection.php
+++ src/Parm/Collection.php
@@ -2,13 +2,19 @@
 
 namespace Parm;
 
+/**
+ * Class Collection
+ * @package Parm
+ */
 class Collection extends Rows {
 
+	/**
+	 * @param DataAccessObjectFactory $factory
+	 */
 	public function __construct(\Parm\DataAccessObjectFactory $factory) {
 
 		parent::__construct($factory);
 
 	}
-
 
 }
