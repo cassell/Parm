c1f789bc1498897d1ffed908e55d27d170e4bf7d
Andrew Cassell <alc277@gmail.com>
2014-04-18 10:54:11 -0400

    docs

--- src/Parm/Config.php
+++ src/Parm/Config.php
@@ -2,26 +2,48 @@
 
 namespace Parm;
 
+/**
+ * Class Config
+ * @package Parm
+ */
 class Config
 {
+	/**
+	 * @var array
+	 */
 	public static $databases = Array();
 
+	/**
+	 * @param string $name
+	 * @param DatabaseNode $masterNode
+	 */
 	static function addDatabase($name, DatabaseNode $masterNode)
 	{
 		static::$databases[$name] = new Database();
 		static::$databases[$name]->setMaster($masterNode);
 	}
 
+	/**
+	 * @param $name
+	 * @return Database
+	 */
 	static function getDatabase($name)
 	{
 		return static::$databases[$name];
 	}
 
+	/**
+	 * @param $name
+	 * @return Database
+	 */
 	static function getDatabaseMaster($name)
 	{
 		return static::$databases[$name]->getMaster();
 	}
 
+	/**
+	 * @return Database
+	 */
 	static function __getFirstDatabaseMaster()
 	{
 		return static::getDatabaseMaster(reset(array_keys(static::$databases)));
