7329a72159d7c3879158f1190fa043adc2517db1
Andrew Cassell <cassell@users.noreply.github.com>
2015-08-22 15:34:49 -0400

    moved bindings in the global namespace to helpers folder

---
+++ helpers/bindings_global_namespace.php
@@ -0,0 +1,17 @@
+<?php
+
+class_alias('Parm\Binding\SQLString', 'SQLString');
+class_alias('Parm\Binding\Conditional\AndConditional', 'AndConditional');
+class_alias('Parm\Binding\Conditional\OrConditional', 'OrConditional');
+class_alias('Parm\Binding\Binding', 'Binding');
+class_alias('Parm\Binding\StringBinding', 'StringBinding');
+class_alias('Parm\Binding\CaseSensitiveEqualsBinding', 'CaseSensitiveEqualsBinding');
+class_alias('Parm\Binding\ContainsBinding', 'ContainsBinding');
+class_alias('Parm\Binding\EqualsBinding', 'EqualsBinding');
+class_alias('Parm\Binding\FalseBooleanBinding', 'FalseBooleanBinding');
+class_alias('Parm\Binding\ForeignKeyObjectBinding', 'ForeignKeyObjectBinding');
+class_alias('Parm\Binding\InBinding', 'InBinding');
+class_alias('Parm\Binding\NotInBinding', 'NotInBinding');
+class_alias('Parm\Binding\NotEqualsBinding', 'NotEqualsBinding');
+class_alias('Parm\Binding\TrueBooleanBinding', 'TrueBooleanBinding');
+class_alias('Parm\Binding\DateBinding', 'DateBinding');
