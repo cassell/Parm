65c79f295639b5685dda1004073a1e215944daf9
Andrew Cassell <cassell@users.noreply.github.com>
2015-10-05 16:18:26 -0400

    enable wake and sleep for DataAccessObjectFactories, disable for DatabaseProcessors

--- src/DataAccessObjectFactory.php
+++ src/DataAccessObjectFactory.php
@@ -767,4 +767,36 @@ abstract class DataAccessObjectFactory extends DatabaseProcessor implements Tabl
         }
     }
 
+    /**
+     * serialize() checks if your class has a function with the magic name __sleep.
+     * If so, that function is executed prior to any serialization.
+     * It can clean up the object and is supposed to return an array with the names of all variables of that object that should be serialized.
+     * If the method doesn't return anything then NULL is serialized and E_NOTICE is issued.
+     * The intended use of __sleep is to commit pending data or perform similar cleanup tasks.
+     * Also, the function is useful if you have very large objects which do not need to be saved completely.
+     *
+     * @return array|NULL
+     * @link http://php.net/manual/en/language.oop5.magic.php#language.oop5.magic.sleep
+     */
+    function __sleep()
+    {
+        // reconnect to database
+        parent::__construct(static::getDatabaseName());
+    }
+
+    /**
+     * unserialize() checks for the presence of a function with the magic name __wakeup.
+     * If present, this function can reconstruct any resources that the object may have.
+     * The intended use of __wakeup is to reestablish any database connections that may have been lost during
+     * serialization and perform other reinitialization tasks.
+     *
+     * @return void
+     * @link http://php.net/manual/en/language.oop5.magic.php#language.oop5.magic.sleep
+     */
+    function __wakeup()
+    {
+        return ['fields','conditional','joinClause','groupByClause','orderByClause','limitClause'];
+    }
+
+
 }
--- src/DatabaseProcessor.php
+++ src/DatabaseProcessor.php
@@ -289,4 +289,36 @@ class DatabaseProcessor
         return $dp->escapeString($string);
     }
 
+    /**
+     * serialize() checks if your class has a function with the magic name __sleep.
+     * If so, that function is executed prior to any serialization.
+     * It can clean up the object and is supposed to return an array with the names of all variables of that object that should be serialized.
+     * If the method doesn't return anything then NULL is serialized and E_NOTICE is issued.
+     * The intended use of __sleep is to commit pending data or perform similar cleanup tasks.
+     * Also, the function is useful if you have very large objects which do not need to be saved completely.
+     *
+     * @return array|NULL
+     * @link http://php.net/manual/en/language.oop5.magic.php#language.oop5.magic.sleep
+     */
+    function __sleep()
+    {
+        throw new \RuntimeException("You can not serialize a DatabaseProcessor because it contains an open database connection.");
+    }
+
+    /**
+     * unserialize() checks for the presence of a function with the magic name __wakeup.
+     * If present, this function can reconstruct any resources that the object may have.
+     * The intended use of __wakeup is to reestablish any database connections that may have been lost during
+     * serialization and perform other reinitialization tasks.
+     *
+     * @return void
+     * @link http://php.net/manual/en/language.oop5.magic.php#language.oop5.magic.sleep
+     */
+    function __wakeup()
+    {
+        throw new \RuntimeException("Unable to deserialize DatabaseProcessor because it requires a database connection.");
+    }
+
+
+
 }
